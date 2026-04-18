493f0ebce818f925c9f248041a264197b670b5c5
LEON-72 <hr.leon72@gmail.com>
2026-04-18 16:26:26 +0900

    feat: 防弾ガラスに当たった時の衝撃

---
+++ Assets/Project/Scripts/Stage/BulletproofGlass.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BulletproofGlass : MonoBehaviour
+{
+    [SerializeField] private GameObject crackPrefab;
+
+    public void AddCrack(Vector3 hitPoint, Vector3 hitNormal)
+    {
+        if (crackPrefab != null)
+        {
+            Vector3 spawnPos = hitPoint + hitNormal * 0.001f;
+
+            Quaternion spawnRot = Quaternion.LookRotation(-hitNormal);
+
+            GameObject crack = Instantiate(crackPrefab, spawnPos, spawnRot, transform);
+
+            crack.transform.Rotate(0, 0, Random.Range(0f, 360f));
+
+            Debug.Log("Glass crack");
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Stage/CrackFadeOut.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class CrackFadeOut : MonoBehaviour
+{
+    private void Start()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+        if (meshRenderer != null && meshRenderer.material != null)
+        {
+            meshRenderer.material.DOFade(0f, 1.0f)
+                .SetDelay(1.0f)
+                .OnComplete(() => Destroy(gameObject));
+
+            if (meshRenderer.material.HasProperty("_EmissionColor"))
+            {
+                meshRenderer.material.DOColor(Color.black, "_EmissionColor", 1.0f)
+                    .SetDelay(1.0f);
+            }
+        }
+    }
+}
