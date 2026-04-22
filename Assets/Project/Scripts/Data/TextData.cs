5291259b3c8c396ba980f3ac73bd3a87ccf5ae5b
LEON-72 <hr.leon72@gmail.com>
2026-04-22 14:59:52 +0900

    feat: 文字を表示するようにした

---
+++ Assets/Project/Scripts/Data/TextData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TextData", menuName = "ScriptableObjects/TextData")]
+public class TextData : ScriptableObject
+{
+    public string gameStartMission = "Escape the lab within 3 minutes!";
+    public float gameStartDisplayTime = 4.0f;
+
+    public string interactPrompt = "[X] : Talk";
+    public string rescueSuccessMessage = "Success!";
+    public float rescueMessageDisplayTime = 3.0f;
+
+    public string npcDeathMessage = "Casualties among survivors";
+    public float npcDeathDisplayTime = 3.0f;
+}
