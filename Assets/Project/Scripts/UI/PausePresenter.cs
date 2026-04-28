57e049cf20888f1d105b602e2fc28ff5e02e47e9
LEON-72 <hr.leon72@gmail.com>
2026-04-28 18:17:00 +0900

    feat: ポーズ画面作成

---
+++ Assets/Project/Scripts/UI/PausePresenter.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PausePresenter : MonoBehaviour
+{
+    [SerializeField] private PauseView view;
+    [SerializeField] private InputActionReference pauseAction;
+
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        view.OnResumeClicked += ResumeGame;
+        view.OnSettingsClicked += OpenSettings;
+        view.OnTitleClicked += GoToTitle;
+    }
+
+    private void OnEnable()
+    {
+        if (pauseAction != null)
+        {
+            pauseAction.action.Enable();
+            pauseAction.action.performed += OnPauseActionPerformed;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (pauseAction != null)
+        {
+            pauseAction.action.performed -= OnPauseActionPerformed;
+        }
+    }
+
+    private void OnPauseActionPerformed(InputAction.CallbackContext context)
+    {
+        TogglePause();
+    }
+
+    private void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    private void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        view.Show();
+    }
+
+    private void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        view.Hide();
+
+        UIEvents.OnPauseStateChanged?.Invoke(false);
+    }
+
+    private void OpenSettings()
+    {
+        Debug.Log("設定画面を開く");
+    }
+
+    private void GoToTitle()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Title");
+    }
+}
---
+++ Assets/Project/Scripts/UI/PauseView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseView : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject defaultSelectedButton;
+
+    public event Action OnResumeClicked;
+    public event Action OnSettingsClicked;
+    public event Action OnTitleClicked;
+
+    [Header("Buttons")]
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        // ボタンのクリックイベントを紐付け
+        resumeButton.onClick.AddListener(() => OnResumeClicked?.Invoke());
+        settingsButton.onClick.AddListener(() => OnSettingsClicked?.Invoke());
+        quitButton.onClick.AddListener(() => OnTitleClicked?.Invoke());
+
+        Hide();
+    }
+
+    public void Show()
+    {
+        pausePanel.SetActive(true);
+
+        if (defaultSelectedButton != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(defaultSelectedButton);
+        }
+    }
+
+    public void Hide()
+    {
+        pausePanel.SetActive(false);
+    }
+}
